3a9d354c30888e73bfe6d755ed546e76a6f1da75
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-03-15 14:36:14 -0400

    Finishing the start page

--- interaction.js
+++ interaction.js
@@ -8,18 +8,16 @@ const topRight = document.querySelector('.top-right');
 const bottomRight = document.querySelector('.bottom-right');
 const bottomLeft = document.querySelector('.bottom-left');
 const grid = Array.from(document.querySelectorAll('.grid'))
+
 // Start the game
-// Remove the start button
-// Insert the counter
-// Insert the reset button
-// Transform all the grids in white background
 function startGame() {
   startButton.style.display = "none";
   counter.style.display = "inline";
   resetButton.style.display = "inline";
+
   grid.forEach(element => {
     element.style.background = "white";
+    element.style.cursor = "pointer";
   })
-}
-
+};
 startButton.addEventListener('click', startGame);
