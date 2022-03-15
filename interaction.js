ad360736a0b8e83931d39c411e55974a8ff07168
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-03-15 15:33:21 -0400

    Adding functionality to change layout when the user start the game and restart it

--- interaction.js
+++ interaction.js
@@ -2,7 +2,7 @@ console.log('Im live')
 // Acess DOM nodes
 const startButton = document.querySelector('.start');
 const counter = document.querySelector('.round-counter');
-const resetButton = document.querySelector('.restart');
+const restartButton = document.querySelector('.restart');
 const topLeft = document.querySelector('.top-left');
 const topRight = document.querySelector('.top-right');
 const bottomRight = document.querySelector('.bottom-right');
@@ -13,11 +13,28 @@ const grid = Array.from(document.querySelectorAll('.grid'))
 function startGame() {
   startButton.style.display = "none";
   counter.style.display = "inline";
-  resetButton.style.display = "inline";
-
+  restartButton.style.display = "inline";
   grid.forEach(element => {
-    element.style.background = "white";
+    element.classList.add('white');
     element.style.cursor = "pointer";
   })
 };
 startButton.addEventListener('click', startGame);
+
+// Restart the game
+function restartGame() {
+  startButton.style.display = "inline";
+  counter.style.display = "none";
+  restartButton.style.display = "none";
+  grid.forEach(element => {
+    element.classList.remove('white');
+  })
+}
+
+restartButton.addEventListener('click', restartGame);
+
+// Generate random numbers
+function randomInt() {
+  randomNumber = Math.floor(Math.random() * grid.length)
+  return randomNumber;
+};
