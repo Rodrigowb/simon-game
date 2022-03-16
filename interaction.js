70d88d5656443d86e7a9b2b8bc607f57e0923056
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-03-16 16:49:31 -0400

    Working on the final adjustments to get the game working

--- interaction.js
+++ interaction.js
@@ -11,6 +11,11 @@ const grid = Array.from(document.querySelectorAll('.grid'))
 
 // Create control variables
 let isGameOn = false;
+let canClick = false;
+// Start a sequence with a initial panel (will change)
+const sequence = [randomPanel()];
+// Keep track of what the user is guessing
+let guessSequence = [...sequence];
 
 // Start the game
 function startGame() {
@@ -21,8 +26,7 @@ function startGame() {
     element.style.cursor = "pointer";
   })
   isGameOn = true;
-  console.log(isGameOn);
-  main();
+  flashingStart();
 };
 startButton.addEventListener('click', startGame);
 
@@ -38,25 +42,66 @@ function restartGame() {
 }
 restartButton.addEventListener('click', restartGame);
 
+// Create random panels
+function randomPanel() {
+  return grid[parseInt(Math.random() * grid.length)];
+}
+
 // Use promisses to wrap timeout
 const flash = panel => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     panel.className += ' white';
+    // Toggle the color
     setTimeout(() => {
       panel.className = panel.className.replace(
         ' white',
         ''
       );
-      resolve();
-    }, 500)
+      // Handle when the same pannel is triggered more than once in a row
+      setTimeout(() => {
+        resolve();
+      }, 250);
+    }, 1000)
   });
 };
 
-// Loop throught the grids
-const main = async () => {
-  while (isGameOn) {
-    for (const panel of grid) {
-      await flash(panel);
+// Check if you click on the right pannel
+function panelCheck(panelClicked) {
+  if (canClick) {
+    const expectedPanel = guessSequence.shift();
+    if (expectedPanel === panelClicked) {
+      // Check if the user clicked right
+      if (guessSequence.length === 0) {
+        // Start new round
+        sequence.push(randomPanel());
+        guessSequence = [...sequence];
+        flashingStart();
+      }
+    } else {
+      // End game
+      alert('Game over');
     }
   }
 }
+
+// Start toggling the grid colors
+const flashingStart = async () => {
+  canClick = false;
+  for (const panel of sequence) {
+    await flash(panel);
+  }
+  canClick = true;
+}
+
+flashingStart();
+
+
+// Event handler when click in the grid
+function clickPanel(event) {
+  return event.target;
+}
+
+// Event listener
+grid.forEach(element => {
+  element.addEventListener('click', clickPanel);
+})
